3bc6b49c72ebb6bc75fbcc4e050cd00c72ce3e38
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-06-13 16:13:21 +0200

    modify Vips to vipsList

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 //check console log
 console.log('ok js');
 //array degli invitati
-const Vips = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+const vipsList = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
 //check vips
-console.log(Vips)
+console.log(vipsList)
 // creo un nuomo arrray contenente degli oggetti 
-const vipsTable = Vips.map((element, index) => {
+const vipsTable = vipsList.map((element, index) => {
 
     return {
         'nome del tavolo' : 'Tavolo Vip',
